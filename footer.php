0b4c9dd06e286fab683e9bf9a9b6cd6dbebd64db
Elouaji <64604839+Elouaji@users.noreply.github.com>
2020-05-12 02:24:05 +0200

    log

---
+++ footer.php
@@ -0,0 +1,27 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" type="image/png" href="fav.jpeg">
+</head>
+<body>
+
+
+
+	<footer>
+		<div class="footer">  
+			
+				
+			<h5>
+				&copy;Gestion d'abcence 2020
+			</h5>
+		</div>
+	</footer>
+
+</body>
+</html>
+
+
--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>Home</title>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="login.css">
 	<link rel="shortcut icon" type="image/png" href="fav.jpeg">
 </head>
 <body>
@@ -27,16 +27,38 @@ Conexion</a>
 			</ul>
 			
 		</div>
-		<div class="title">
-			<img src="2.png">
-			<p></p>
-		</div>
-		<div class="button">
-			<a href="login.php" class="btn"><i class="fas fa-sign-in-alt"></i>Se connecter</a>
-			
-		</div>
+		
+		
 
 	</header>
-	</body>
+	<footer>
+		<div class="footer">  
+			
+				
+			<h5>
+				&copy;Gestion d'abcence 2020
+			</h5>
+		</div>
+	</footer>
+
+</body>
 </html>
 
+
+</div>
+	<div  class="loginbox"> 
+	<img src="avatar.png" class="avatar">
+	<h1> Connexion</h1>
+	<form>
+		<p> username</p>
+		<input type="text" name="" placeholder=" username">
+		<p> mot de passe</p>
+		<input type="password" name="" placeholder=" mot de passe">
+		<input type="submit" name="" value="connecter">
+		<a href="signup.php">Vous n'avez pas de données ? contactez l'université!</a><br>
+		
+		</form>
+	
+
+</body>
+</html>
